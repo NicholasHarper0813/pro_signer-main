926b851a5db9a54f68040ce50bff8d661a1e527c
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 06:45:25 -0800

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,6 @@
 import { SupabaseClient, Session } from "@supabase/supabase-js"
 import { Database } from "./DatabaseDefinitions"
 
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
   namespace App {
     interface Locals {
@@ -13,8 +11,6 @@ declare global {
     interface PageData {
       session: Session | null
     }
-    // interface Error {}
-    // interface Platform {}
   }
 }
 
